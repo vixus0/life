fcce1eec283659d1cf4b495471f725bed3afe313
Anshul Sirur <anshul.sirur@digital.cabinet-office.gov.uk>
2016-02-19 17:24:13 +0000

    Bit more efficient?

--- life.py
+++ life.py
@@ -2,10 +2,10 @@ import numpy as np
 from Tkinter import *
 
 def count_live(grid, point, radius=1, wrap=False):
-    pad_mode = 'wrap' if wrap else 'constant'
-    padded = np.pad(grid, radius, mode=pad_mode, constant_values=0)
-    x, y = map(lambda a: a+1, point)
-    return np.sum(padded[x-radius:x+radius+1, y-radius:y+radius+1]) - padded[x,y]
+    x, y = point
+    xslice = slice(max(0, x-radius), min(grid.shape[0], x+radius+1))
+    yslice = slice(max(0, y-radius), min(grid.shape[1], y+radius+1))
+    return np.sum(grid[xslice, yslice]) - grid[x,y]
 
 def count_grid(grid):
     neighbours = np.zeros(grid.shape)
@@ -23,34 +23,31 @@ def iterate(start):
 
 class Animation(object):
     def __init__(self):
-        self.rects = []
-        self.size = 5
-        self.delay = 10
-        self.grid = np.random.randint(0, 2, size=(100,100))
+        self.colors = {1:'black', 0:'white'}
+        self.rects = {}
+        self.size = 4
+        self.delay = 0
+        self.grid = np.random.randint(0, 2, size=(50,50))
         dim = self.grid.shape
         self.root = Tk()
         self.canvas = Canvas(self.root, width=dim[0]*self.size, height=dim[1]*self.size)
+        self.init_rects()
         self.canvas.pack()
         self.root.after(self.delay, self.step)
         self.root.mainloop()
 
+    def init_rects(self):
+        for point in np.ndindex(self.grid.shape):
+            x, y = point
+            coords = (x*self.size, y*self.size, (x+1)*self.size, (y+1)*self.size)
+            self.rects[point] = self.canvas.create_rectangle(
+                    *coords, fill='white', width=0
+                    )
+
     def step(self):
         size = self.size
-        self.canvas.delete(ALL)
-        self.new_rects = []
         for point in np.ndindex(self.grid.shape):
-            if self.grid[point] == 1:
-                x, y = point
-                if len(self.rects) == 0:
-                    r = self.canvas.create_rectangle(x*size, y*size, (x+1)*size, (y+1)*size, fill='black')
-                else:
-                    r = self.rects.pop()
-                    c = np.array(self.canvas.coords(r)[:2])
-                    nc = np.array([x*size, y*size])
-                    offs = c - nc
-                    c.move(r, offs[0], offs[1])
-                self.new_rects.append(r)
-        self.rects = self.new_rects
+            self.canvas.itemconfig(self.rects[point], fill=self.colors[self.grid[point]])
         self.grid = iterate(self.grid)
         self.root.update()
         self.root.after(self.delay, self.step) 
